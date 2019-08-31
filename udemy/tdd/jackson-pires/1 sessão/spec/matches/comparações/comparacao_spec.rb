f47d1d61a59be144da4aa48040d347d1bf96a39f
jdruk <devjosafa@gmail.com>
2019-08-31 19:52:33 -0300

    Match de comparação (equal/be) e (eql/eq)

---
+++ udemy/tdd/jackson-pires/1 sessão/spec/matches/comparações/comparacao_spec.rb
@@ -0,0 +1,31 @@
+describe "Comparações" do 
+  context "Objecto/Conteúdo" do 
+    it "#equal" do 
+      x = "ruby"
+      y = "ruby"
+      expect(x).not_to equal(y)
+    end
+
+    it "#be" do 
+      # Atalho para equal()
+      x = "ruby"
+      y = "ruby"
+      expect(x).not_to be(y)
+    end
+  end
+
+  context "Conteúdo" do
+    it "#eql" do 
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eql(y)
+    end
+
+    it "#eq" do
+      # Atalho para eql()
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eq(y)
+    end
+  end
+end
