d3b2806e5febfc87062ac2719ca356c4de078897
jdruk <devjosafa@gmail.com>
2019-09-01 14:54:35 -0300

    match composição (and/or)

---
+++ udemy/tdd/jackson-pires/spec/composição/composicao_spec.rb
@@ -0,0 +1,9 @@
+describe "Josafá Martins dos Santos" do 
+  it { is_expected.to start_with('Josafá').and end_with('Santos') }
+
+  it { expect(styles).to eq('rock').or eq('pop').or eq('rapper') }
+
+  def styles 
+    %w(rock pop rapper).sample
+  end
+end
